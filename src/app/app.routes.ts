0969fc23b78ebc1cb3d7348787193da4848a7fd8
mlpz <miguelkrasnob@gmail.com>
2024-09-07 19:26:38 +0200

    Agregadas rutas en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './pages/users-list/users-list.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+import { FormComponent } from './pages/form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: "full", redirectTo: "home"},
+    { path: "home", component: UsersListComponent },
+    { path: "user/:idUser", component: UserViewComponent },
+    { path: "newuser", component: FormComponent },
+    { path: "update/user/:id", component: FormComponent}
+];
