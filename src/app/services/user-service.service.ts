1fd67ec34376f2ab785c01ab192d8a332fcfce49
mlpz <miguelkrasnob@gmail.com>
2024-09-07 22:58:29 +0200

    Generada home con header, users-list component y footer

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { lastValueFrom } from 'rxjs';
-import {  } from 'rxjs';
 import { UserInt } from '../interfaces/user-int.interface';
 
 @Injectable({
@@ -13,17 +11,19 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-    // Traer todos los datos de la API (pagina 1) por medio de promesa:
   getUsers(page: number = 1): Promise<any> {
-    return lastValueFrom(this.http.get<any>(`${this.apiUrl}?page=${page}`))
-
+    return lastValueFrom(this.http.get<UserInt>(`${this.apiUrl}?page=${page}`))
   }
 
   getUserById(id: number): Promise<any> {
     return lastValueFrom(this.http.get<UserInt>(`${this.apiUrl}/${id}`));
   }
 
-  create(formValue: UserInt): Promise<any> {
-    return lastValueFrom(this.http.post<UserInt>(this.apiUrl, formValue))
+  createUser(user: UserInt): Promise<any> {
+    return lastValueFrom(this.http.post<UserInt>(this.apiUrl, user));
+  }
+
+  updateUser(id: number, user: UserInt): Promise<any> {
+    return lastValueFrom(this.http.put<UserInt>(`${this.apiUrl}/${id}`, user));
   }
 }
