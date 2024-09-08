7c68ca96bd5c8523b61a2d21763fd5d27eecfdb6
mlpz <miguelkrasnob@gmail.com>
2024-09-08 17:01:46 +0200

    Visualización de perfil/detalles usuario completada + tamaño de botones en perfil/detalles usuario modificados si parent=='view'

--- src/app/pages/user-view/user-view.component.ts
+++ src/app/pages/user-view/user-view.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { UserService } from '../../services/user-service.service';
+import { ButtonsComponent } from '../../components/buttons/buttons.component';
+import { UserInt } from '../../interfaces/user-int.interface';
 
 @Component({
   selector: 'app-user-view',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ButtonsComponent],
   templateUrl: './user-view.component.html',
   styleUrl: './user-view.component.css'
 })
-export class UserViewComponent {
+export class UserViewComponent implements OnInit {
 
+  userService = inject(UserService)
+  activatedRoute = inject(ActivatedRoute)
+  userData: UserInt | undefined;
+
+  async ngOnInit(): Promise<void> {
+    const _id = this.activatedRoute.snapshot.paramMap.get('_id');
+    if (_id) {
+      try {
+        this.userData = await this.userService.getUserById(_id);
+      } catch (error) {
+        console.log("No se ha podido recuperar el usuario", error)
+      }
+    }
+  }
 }
