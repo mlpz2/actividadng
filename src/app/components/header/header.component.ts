e62d9718ba9cc0ef5192d472702e17932988ab0f
mlpz <miguelkrasnob@gmail.com>
2024-09-08 11:35:50 +0200

    Agregado header con título, botones homes y nuevo usuario y estilos correspondientes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
+
 export class HeaderComponent {
 
+  router = inject(Router);
+
+  ir(url: String) {
+    this.router.navigate([url]);
+  }
 }
